85d2289be005bf88fbecb972b98bec1c020095bb
Tom Dyer <tdyer@ourstage.com>
2014-05-13 06:58:59 -0400

    Use user_signed_in? instead of current_user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,8 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    if current_user
+    if user_signed_in?
+      binding.pry
       @articles = current_user.articles
     else
       @articles = Article.all
@@ -70,7 +71,7 @@ class ArticlesController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_article
-    if current_user
+    if user_signed_in?
       @article = current_user.articles.find(params[:id])
     else
       @article = Article.find(params[:id])
